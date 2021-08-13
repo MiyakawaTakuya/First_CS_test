c31063d4b6779f5e7fd5086e57e9d00bf07d03d7
Miyakawa Takuya <omyakawa@gmail.com>
2021-08-13 20:41:57 +0900

    My First C#

---
+++ FirstCSr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCSr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# !" + "Hasta La Vista, baby!!");
+        }
+    }
+}
