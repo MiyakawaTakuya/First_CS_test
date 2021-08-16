1cfff7566673bdf98215f952e4c031f8a814bb34
Miyakawa Takuya <omyakawa@gmail.com>
2021-08-16 23:54:25 +0900

    追従型カメラ

---
+++ FirstCSr/PlayerFollower.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerFollowe : MonoBehaviour {  
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {  //プレイボタン押したときに一回だけ実行される
+		offset = new Vector3(0,0,1);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {  //１フレーム６０回描画している
+        
+		
+    }
+}
