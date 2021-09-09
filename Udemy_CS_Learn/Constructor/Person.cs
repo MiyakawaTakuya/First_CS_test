cb0e8f8ff38d79a5e1a56ef6245d19b74a020334
Miyakawa Takuya <omyakawa@gmail.com>
2021-09-10 02:18:16 +0900

    Constructor,Overloadについて演習した

---
+++ Udemy_CS_Learn/Constructor/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Person
+    {
+        //フィールドはプライベート　クラス内のデータ管理  ここには他からアクセスできない
+        private string _firstname;
+        private string _lastname;
+        private int _age = 26;
+        //private int _age;
+
+        //オーバーロード
+        public Person()
+        {
+            _firstname = "Taro";
+            _lastname = "Domyoji";
+        }
+
+
+        //２つのパラメーター(引数)を用意している
+        public Person(string firstname, int age)
+        {
+            _firstname = firstname;
+            _age = age;
+        }
+
+        //２つのパラメーター(string型の引数)を用意している
+        public Person(string firstname, string lastname) 
+        {
+            _firstname = firstname;
+            _lastname = lastname;
+        }
+
+        //３つのパラメーター(string型の引数)を用意している
+        public Person(string firstname, string lastname, int age)
+        {
+            _firstname = firstname;
+            _lastname = lastname;
+            _age = age;
+        }
+
+
+        //年齢が入力された場合とそうでない場合  オーバーロード　
+        public string GetFullnameWithAge(int age)
+        {
+            return $"{_firstname} {_lastname}  {age}";  //GetFullnameWithAgeの引数で渡された年齢になる
+        }
+
+        public string GetFullnameWithAge()
+        {
+            return $"{_firstname} {_lastname}  {_age}";
+        }
+
+        //public string GetFullname()
+        //{
+        //    return $"{_firstname} {_lastname}";
+        //}
+    }
+}
--- Udemy_CS_Learn/Constructor/Program.cs
+++ Udemy_CS_Learn/Constructor/Program.cs
@@ -1,24 +1,33 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Constructor  //コンストラクターについてのLearn
 {
-    
-    class Person
-    {
-        private string _firstname;
-        private string _lasttname;
+    class Program
+    { 
+       static void Main(string[] args)
+       {
+            Person defaultPerson = new Person();
+            Console.WriteLine(defaultPerson.GetFullnameWithAge()); //Taro Domyoji 26
 
-        public Person(string firstname, string lastname)
-        {
-            _firstname = firstname;
-            _lasttname = lastname;
+            Person person1 = new Person("Takuya", "Miyakawa");
+            Console.WriteLine(person1.GetFullnameWithAge()); //Takuya Miyakawa  26
+            Console.WriteLine(person1.GetFullnameWithAge(18)); //Hikari Amamiya  18
 
-        }
+            Person person2 = new Person("Hikari", "Amamiya");
+            Console.WriteLine(person2.GetFullnameWithAge());  //Hikari Amamiya  26
+            Console.WriteLine(person2.GetFullnameWithAge(18));  //Hikari Amamiya  18
 
-        public string GetFllname()
-        {
-            return $""
+            Person person3 = new Person("Hikari", 32);
+            Console.WriteLine(person3.GetFullnameWithAge()); //Hikari 32
+            Console.WriteLine(person3.GetFullnameWithAge(18));  //Hikari 18
+
+            Person person4 = new Person("Takuya", "Miyakawa",35);
+            Console.WriteLine(person4.GetFullnameWithAge());  //Takuya Miyakawa  35
+            Console.WriteLine(person4.GetFullnameWithAge(26));  //Takuya Miyakawa 26
         }
     }
-
 }
