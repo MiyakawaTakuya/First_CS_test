fc1767d953abefcde83c0fa78b517220813be875
Miyakawa Takuya <omyakawa@gmail.com>
2021-09-09 20:59:52 +0900

    Constructorについて学習

---
+++ Udemy_CS_Learn/CS07/EmptyInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CS07
+{
+    public interface EmptyInterface
+    {
+        public void cs11()
+        {
+            //int aaa = 1;
+
+            //int[] values = new int[3]; //[]のなかに作りたい個数を記載
+            //values[0] = 10;
+            //values[1] = 100;
+            //values[2] = 999;
+            //Console.WriteLine(values);
+            //Console.WriteLine(values.Length.ToString());
+
+            //int[] values2 = { 1, 2, 3, 4, 5, 6 };
+            //Console.WriteLine(values2.Length.ToString());
+
+            var names = new List<string> { "a","b",""};
+
+            foreach (var item in names)
+            {
+                if (item.Length == 0)
+                    break;
+            }
+
+            private void list()
+            {
+                List<>
+            }
+        }
+
+        
+       
+
+        
+
+    }
+}
---
+++ Udemy_CS_Learn/Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Constructor  //コンストラクターについてのLearn
+{
+    
+    class Person
+    {
+        private string _firstname;
+        private string _lasttname;
+
+        public Person(string firstname, string lastname)
+        {
+            _firstname = firstname;
+            _lasttname = lastname;
+
+        }
+
+        public string GetFllname()
+        {
+            return $""
+        }
+    }
+
+}
